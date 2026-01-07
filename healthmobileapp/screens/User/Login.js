407d2ed5228bdf1e05f458d92438746a7b62a875
Nguyễn Duy Hoàng <Nguyenduyhoang0605@gmail.com>
2026-01-07 21:08:55 +0700

    update healthProfile

--- healthmobileapp/screens/User/Login.js
+++ healthmobileapp/screens/User/Login.js
@@ -7,7 +7,7 @@ import Apis, { authApis, endpoints } from "../../utils/Apis";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MyUserContext } from "../../utils/MyContexts";
 
-const Login = ({route}) => {
+const Login = ({ route }) => {
     const info = [{
         "label": "Tên đăng nhập",
         "field": "username",
@@ -23,7 +23,7 @@ const Login = ({route}) => {
     const [errMsg, setErrMsg] = useState();
     const [loading, setLoading] = useState(false);
     const nav = useNavigation();
-    const [,dispatch] = useContext(MyUserContext);
+    const [, dispatch] = useContext(MyUserContext);
 
     const validate = () => {
         if (!user.username) {
@@ -46,41 +46,41 @@ const Login = ({route}) => {
         try {
             setLoading(true);
 
-    const data = new URLSearchParams();
-    data.append("username", user.username);
-    data.append("password", user.password);
-    data.append("client_id","lOLoyDZLInReEtLer9NuBLefZpwiy1NgGpz2GVQ7");
-    data.append("client_secret", "1i5JrNK8ryHzBwwWV1OpzR3w3I7hGYe8N15AB826o9B6nxa50oIci7NNaf0NNE9ZSBTpP117Kfu4weQleOEL4aNX5ZcKua1b1r2xfDFYTEEZGZcr4AabYjAJrG12Ei3Z");
-    data.append("grant_type", "password");
+            const data = new URLSearchParams();
+            data.append("username", user.username);
+            data.append("password", user.password);
+            data.append("client_id", "fxGJPzHmHiVphQSJ0eTviLBz5UCiVP4qUUSathjL");
+            data.append("client_secret", "8OvvDFiwi8kHteejHqkGrYKKL9Yz8K7ji9FLqfad4yGdad4tOv1n4txYwRgRTvWjjzbSDGKrLSNsBqodL23SlRJN0S2DbJpVw3i3FdeALo00uZRa2Qmc18521KMwImXK");
+            data.append("grant_type", "password");
 
-    const res = await Apis.post(endpoints['login'], data, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    });
+            const res = await Apis.post(endpoints['login'], data, {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            });
 
-    console.log("TOKEN:", res.data);
+            console.log("TOKEN:", res.data);
 
-    await AsyncStorage.setItem("access_token", res.data.access_token);
+            await AsyncStorage.setItem("access_token", res.data.access_token);
 
-    await AsyncStorage.setItem("refresh_token", res.data.refresh_token);
+            await AsyncStorage.setItem("refresh_token", res.data.refresh_token);
 
-    const u = await authApis(res.data.access_token)
-      .get(endpoints['current-user']);
+            const u = await authApis(res.data.access_token)
+                .get(endpoints['current-user']);
 
-    dispatch({
-      type: "login",
-      payload: u.data
-    });
+            dispatch({
+                type: "login",
+                payload: u.data
+            });
 
-    nav.navigate("ChooseMode");
+            nav.navigate("ChooseMode");
 
-    } catch (err) {
-        console.log("LOGIN ERROR:", err.response?.data);
-        setErrMsg("Sai tài khoản hoặc mật khẩu!");
-    } finally {
-        setLoading(false);
-    }
+        } catch (err) {
+            console.log("LOGIN ERROR:", err.response?.data);
+            setErrMsg("Sai tài khoản hoặc mật khẩu!");
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
@@ -92,14 +92,14 @@ const Login = ({route}) => {
             </HelperText>
 
             {info.map(i => <TextInput key={i.field} style={MyStyles.margin}
-                            label={i.label}
-                            secureTextEntry={i.secureTextEntry}
-                            right={<TextInput.Icon icon={i.icon} />}
-                            value={user[i.field]}
-                            onChangeText={t => setUser({...user, [i.field]: t})} />)}
+                label={i.label}
+                secureTextEntry={i.secureTextEntry}
+                right={<TextInput.Icon icon={i.icon} />}
+                value={user[i.field]}
+                onChangeText={t => setUser({ ...user, [i.field]: t })} />)}
 
-          
-            {user.avatar && <Image  source={{uri: user.avatar.uri}} width={250} style={[MyStyles.avatar, MyStyles.margin]} />}
+
+            {user.avatar && <Image source={{ uri: user.avatar.uri }} width={250} style={[MyStyles.avatar, MyStyles.margin]} />}
 
             <Button loading={loading} disabled={loading} mode="contained" icon="account" onPress={login}>Đăng nhập</Button>
 
--- healthmobileapp/utils/Apis.js
+++ healthmobileapp/utils/Apis.js
@@ -1,15 +1,14 @@
 import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE_URL = 'http://10.106.121.88:8000/';
-// const BASE_URL = 'http://192.168.1.5:8000/';
+// const BASE_URL = 'http://10.106.121.88:8000/';
+const BASE_URL = 'http://192.168.1.5:8000/';
 
 export const endpoints = {
     'register': '/users/',
     'login': '/o/token/',
     'current-user': '/users/current-user/',
-    'exercises':'/exercises/',
-    'exercises_plans':'/exercises_plans/',
+    'exercises': '/exercises/',
+    'exercises_plans': '/exercises_plans/',
     'add_exercise_to_plan': (planId) => `/exercises_plans/${planId}/exercises/`,
     'health_journals': '/HealthJournal/',
     'health_profile': '/healthprofiles/',
